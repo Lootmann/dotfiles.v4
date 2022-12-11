0cdac475433c5b3c97a377f66a0decc59ef2ff9e
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-12-11 14:24:13 +0900

    copy.sh: new copy commands

--- copy.sh
+++ copy.sh
@@ -4,6 +4,9 @@ cp ~/.vimrc .
 cp ~/.zshrc .
 cp ~/.tmux.conf .
 cp ~/.config/alacritty/alacritty.yml .
+
+mkdir ./nvim
 cp ~/.config/nvim/init.lua ./nvim/init.lua
-cp ~/.config/nvim/lua      ./nvim/lua/ -r
-cp ~/.config/nvim/snippets ./nvim/snippets/ -r
+cp ~/.config/nvim/lua/      ./nvim/lua/ -r
+cp ~/.config/nvim/snippets/ ./nvim/snippets/ -r
+rm -r ./nvim/lua/.config/
